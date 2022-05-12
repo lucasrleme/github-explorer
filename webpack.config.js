e33c304692565d55e288e36f7bddb14b9c306d19
Lucas Leme <lucasratm40@gmail.com>
2022-05-12 00:04:51 -0300

    Config react and automatic react import in babel

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  //entry: 'src/index.jsx'
+  entry: path.resolve(__dirname, 'src', 'index.jsx'), //qual é o arquivo principal, inicial da aplicação
+  output:{
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module:{ //define o comportamento dos loaders para cada tipo de arquivo
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/, //node_modules já está pronto para o browser
+        use: 'babel-loader'
+      }
+    ],
+  }
+};
