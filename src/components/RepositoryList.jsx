14ed51d5448ebe99af90280c5e65aa03cf10eacf
Lucas Leme <lucasratm40@gmail.com>
2022-05-13 04:09:20 -0300

    Start to use props concept  in react

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,20 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'Unform',
+  description: 'Forms in React',
+  link: 'https://github.com/lucasrleme'
+}
+
+export function RepositoryList(){
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <RepositoryItem repository={repository} /> 
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+      </ul>
+    </section>
+  );
+}
