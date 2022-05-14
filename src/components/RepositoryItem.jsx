8128da3c77a3dd9f6a980ed1f30c4eddb35c670c
Lucas Leme <lucasratm40@gmail.com>
2022-05-14 01:18:12 -0300

    Request the github api data from our user to list the repositories

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,11 @@
 export function RepositoryItem(props){
+  //name , description and html_url is the names of github objects api
   return (
     <li>
-      <strong>{props.repository.name}</strong>
+      <strong>{props.repository.name}</strong> 
       <p>{props.repository.description}</p>
 
-      <a href={props.repository.link}>
+      <a href={props.repository.html_url}>
         Acessar repositório
       </a>
       
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,12 +3,6 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useState , useEffect } from "react";
 
-const repository = {
-  name: 'Unform',
-  description: 'Forms in React',
-  link: 'https://github.com/lucasrleme'
-}
-
 export function RepositoryList(){
   const [repositories, setRepositories] = useState([])
 
@@ -22,9 +16,9 @@ export function RepositoryList(){
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository} /> 
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })} 
       </ul>
     </section>
   );
